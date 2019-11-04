0ce9e25f8196a5addfaaed358defa2c68eaf1573
petrarka1488@yandex.ru <petrarka1488@yandex.ru>
2019-11-04 23:05:33 +0300

    коммит

--- ConsoleApp1test/Program.cs
+++ ConsoleApp1test/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
+           
         }
     }
 }
